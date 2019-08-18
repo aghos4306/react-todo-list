fdfc18b58945750aa28c3e6ab3fb99d038103a90
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2019-08-18 21:35:19 +0300

    Structure of project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import TodoInput from './components/TodoInput';
 import TodoItem from './components/TodoItem';
 import TodoList from './components/TodoList';
 
 class App extends Component {
+  state = {
+    items: [],
+    id:uuid(),
+    item:'',
+    editItem: false
+  };
+  handleChange = (e) =>{
+    this.setState({
+      item: e.target.value
+    })
+  };
+  handleSubmit = (e) =>{
+    e.preventDefault();
+    const newItem = {
+      id:this.state.id,
+      title:this.state.item
+    };
+    const updatedItems = [...this.state.items,newItem];   //spread operator
+    this.setState({
+      items:updatedItems,
+      item:'',
+      id:uuid(),
+      editItem:false
+    }, ()=> console.log(this.state));
+  };
+  clearList = () => {
+    console.log('clear list method')
+  };
+  handleDelete = (id) =>{
+    console.log(`handle delete ${id}`);
+  };
+  handleEdit = (id) =>{
+    console.log('handle edit method');
+  };
   render() {
     return (
-      <div>
         <div className="container">
           <div className="row">
-            <TodoInput />
-            <TodoList />
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">todo input</h3>
+              <TodoInput item={this.state.item} handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit} editItem={this.state.editItem}
+              />
+              <TodoList items={this.state.items} clearList={this.clearList}
+              handleDelete={this.handleDelete} handleEdit={this.handleEdit}
+              />
+            </div>
           </div>
         </div>
-      </div>
+        
     );
   }
 }
